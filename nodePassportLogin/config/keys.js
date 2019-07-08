ffd1254fdec646085e3e0921b267d76e6f0c7195
JaniPaasiluoto <43338681+JaniPaasiluoto@users.noreply.github.com>
2019-07-08 19:32:41 +0300

    Update keys.js

--- nodePassportLogin/config/keys.js
+++ nodePassportLogin/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MongoURI: 'mongodb+srv://janipa:jonpula1!@test-cluster1-ptpke.mongodb.net/test?retryWrites=true'
+  MongoURI: 'mongodb+srv://username:password!@test-cluster1-ptpke.mongodb.net/test?retryWrites=true'
 }
